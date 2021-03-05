10aaa661e486ebbcd099cee86013814799e55d22
Jair Valadez <val20013@byui.edu>
2021-03-04 23:10:50 -0600

    Updated the Login.java class.

--- app/src/main/java/com/example/thefriendzone/Login.java
+++ app/src/main/java/com/example/thefriendzone/Login.java
@@ -1,14 +1,85 @@
 package com.example.thefriendzone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    Button buttonCreateProfile, buttonLogin;
+    EditText user_email, password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        buttonCreateProfile=findViewById(R.id.btnCreateAccount);
+        buttonCreateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateProfile.class));
+            }
+        });
+
+        user_email = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.btnLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // extract or validate
+                if(user_email.getText().toString().isEmpty()){
+                    user_email.setError("Email is missing");
+                    return;
+                }
+
+                if(password.getText().toString().isEmpty()){
+                    password.setError("Password is missing");
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(user_email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //Successful Login
+                        startActivity(new Intent(getApplicationContext(), FriendZone.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), FriendZone.class));
+            finish();
+        }
     }
 }
