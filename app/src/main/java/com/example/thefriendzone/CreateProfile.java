09f9bdd82c8186a684e65d71b6a82701d12a03f0
Hyrum Elwood <elwoohyru@gmail.com>
2021-03-06 09:41:57 -0700

    Added category and interest lists

--- app/src/main/java/com/example/thefriendzone/CreateProfile.java
+++ app/src/main/java/com/example/thefriendzone/CreateProfile.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +15,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class CreateProfile extends AppCompatActivity {
     EditText createFirstName, createLastName, createEmailAddress, createPassword, confPassword, profileBio;
