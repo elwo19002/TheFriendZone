7b852a9bbe57872f65de2362893ebd23c675fa69
Jair Valadez <val20013@byui.edu>
2021-03-18 21:35:28 -0600

    Created the logout button

--- app/src/main/java/com/example/thefriendzone/FriendZone.java
+++ app/src/main/java/com/example/thefriendzone/FriendZone.java
@@ -1,14 +1,32 @@
 package com.example.thefriendzone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 /** The friendzone class is the main page for our users. They use this to see their matches and communicate with them.*/
 public class FriendZone extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button logout = findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
     }
 }
